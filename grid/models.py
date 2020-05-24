c416b751e521f0cbb1d5303039f608581fb467d9
Dan Njoroge <greatdaniels@gmail.com>
2020-05-24 13:23:13 +0300

    adding class Location

--- grid/models.py
+++ grid/models.py
@@ -19,3 +19,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name 
+
+class Location(models.Model):
+    location_name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.location_name
