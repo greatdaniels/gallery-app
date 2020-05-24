209db01401c37a15b88f931714666f77dc931125
Dan Njoroge <greatdaniels@gmail.com>
2020-05-24 14:22:59 +0300

    adding Editor tests

--- grid/tests.py
+++ grid/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Editor, Category, Location
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+    def setUp(self):
+        self.james = Editor(editor_name = 'James', email = 'james@gmail.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james, Editor))
+
+    def test_save_method(self):
+        self.james.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
