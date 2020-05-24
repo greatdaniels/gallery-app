474f3a30d236f47304d4a645e2b1934dcccaf353
Dan Njoroge <greatdaniels@gmail.com>
2020-05-24 13:17:24 +0300

    add class Editor

--- grid/models.py
+++ grid/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Editor(models.model):
+    editor_name = models.CharField(max_length = 30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.editor_name
+
+    def save_editor(self):
+        self.save()
+
+    class Meta:
+        ordering = ['editor_name']
