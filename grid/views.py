9e177746637af125b8ece365947da72eb8827f9a
Dan Njoroge <greatdaniels@gmail.com>
2020-05-24 14:06:22 +0300

    db migration

--- grid/views.py
+++ grid/views.py
@@ -3,4 +3,4 @@ from django.http  import HttpResponse
 
 # Create your views here.
 def gallery(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'welcome.html')
